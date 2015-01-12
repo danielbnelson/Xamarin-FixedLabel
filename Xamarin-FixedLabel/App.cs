7dc52948ac50bd5a7d202ad56f5120fba1087b18
Daniel Nelson <daniel.nelson@gmail.com>
2015-01-12 16:24:05 -0500

    initial commmit

---
+++ Xamarin-FixedLabel/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFixedLabel
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+	
+
+			return new ContentPage { 
+			
+				Content = new FixedLabel("Hello, Forms!",100,100) {
+					Text = "Hello, Forms!",
+					TextColor = Color.Blue,
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				},
+
+
+			};
+		}
+	}
+}
+
